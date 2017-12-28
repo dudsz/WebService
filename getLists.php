195a656654381d97af3018e66a5b27f4f4af882e
Markus Erlach <markus.erlach@gmail.com>
2017-12-28 21:24:43 +0100

    FIX: Uppdaterad fetch

--- getLists.php
+++ getLists.php
@@ -2,28 +2,31 @@
 
 require_once 'wishFunctions.php';
 $db = new Testing();
-$jResponse = array("error" => FALSE);
+$jResponse = array("success" => 0);
 
 if (isset($_POST['un']) && !empty($_POST['un'])) {
 	// Set values from post params
 	$username = $_POST['un'];
-	// Login user
+	// User whose list we are getting
 	$wLists = $db->getLists($username);
-	if ($wLists) {		
-		$jResponse["error"] = FALSE;
+	if ($wLists) {
 		$jResponse["success"] = 1;
-		$jResponse["wishes"] = $wLists;
+		$jResponse["msg"] = "Wish lists found";
+		$listNames = array();
+		foreach ($wLists as $wList) {
+			$listNames[] = $wList["wishListName"];
+		}
+		$jResponse["wishLists"] = $listNames;
+		//$jResponse["wishLists"] = $wLists;
 		echo json_encode($jResponse);
 	} else {
-		// Failed to register
-		$jResponse["error"] = FALSE;
+		// Failed to find a list for specifed user
 		$jResponse["success"] = 0;
-		$jResponse["msg"] = "No lists found"; 
+		$jResponse["msg"] = "No lists found";
 		echo json_encode($jResponse);
-	} 
+	}
 } else {
 	// Bad post params, no values set
-	$jResponse["error"] = TRUE;
 	$jResponse["success"] = 0;
 	$jResponse["msg"] = "Bad parameters"; 
 	echo json_encode($jResponse);
