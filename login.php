c71f128838a27aa032bb41a33ea41766699c6ca4
Markus Erlach <markus.erlach@gmail.com>
2017-12-28 16:58:42 +0100

    FIX: Tog bort error json & uppdaterade response

--- login.php
+++ login.php
@@ -2,36 +2,33 @@
 
 require_once 'functions.php';
 $db = new Testing();
-$jResponse = array("error" => FALSE);
+$jResponse = array("success" => 0);
 
 if (isset($_POST['un']) && !empty($_POST['un']) 
-	&& isset($_POST['pw']) && !empty($_POST['pw'])) {
-	// Set values from post params
-	$username = $_POST['un'];
-	$password = $_POST['pw'];
-	// Login user
-	$user = $db->loginUser($username, $password);
-	if ($user) {
-		$jResponse["error"] = FALSE;
-		$jResponse["success"] = 1;
-		$jResponse["msg"] = "Login successful";
-		$jResponse["user"]["username"] = $user["username"];
-		$jResponse["user"]["email"] = $user["email"];
-		$jResponse["user"]["password"] = $user["password"];
-		echo json_encode($jResponse);
-	} else {
-		// Failed to register
-		$jResponse["error"] = FALSE;
-		$jResponse["success"] = 0;
-		$jResponse["msg"] = "User not inserted"; 
-		echo json_encode($jResponse);
-	} 
+        && isset($_POST['pw']) && !empty($_POST['pw'])) {
+        // Set values from post params
+        $username = $_POST['un'];
+        $password = $_POST['pw'];
+        // Login user
+        $user = $db->loginUser($username, $password);
+        if ($user) {
+                $jResponse["success"] = 1;
+                $jResponse["msg"] = "Login successful";
+                $jResponse["user"]["username"] = $user["un"];
+                $jResponse["user"]["email"] = $user["email"];
+                $jResponse["user"]["password"] = $user["pw"];
+                echo json_encode($jResponse);
+        } else {
+                // Wrong username or password
+                $jResponse["success"] = 0;
+                $jResponse["msg"] = "Incorrect username or password"; 
+                echo json_encode($jResponse);
+        } 
 } else {
-	// Bad post params, no values set
-	$jResponse["error"] = TRUE;
-	$jResponse["success"] = 0;
-	$jResponse["msg"] = "Bad parameters"; 
-	echo json_encode($jResponse);
+        // Bad post params, no values set
+        $jResponse["success"] = 0;
+        $jResponse["msg"] = "Bad parameters";
+        echo json_encode($jResponse);
 }
 
 ?>
